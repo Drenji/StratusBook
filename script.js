3c592d92e2abe3f25f626479b5c16f83f25358f9
Drenji <Benjamin.petricoul@gmail.com>
2024-07-18 17:49:52 +0200

    ajout des premiers fichiers, avec header et footer finis (V1)

---
+++ script.js
@@ -0,0 +1,8 @@
+const source_leave = document.getElementById('source_code')
+source_leave.addEventListener("click", () => {
+        user_inp = confirm("This link redirects to GitHub, are you sure you want to leave ?")
+        if (user_inp) {
+            window.location.href = "https://github.com/Drenji"
+        }
+    }
+)
